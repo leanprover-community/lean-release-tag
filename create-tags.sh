038a1222c9e8d5cca15a1e7b06ed375dc9087bd8
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-05-20 10:17:59 +0200

    Improve error and warning messages

--- create-tags.sh
+++ create-tags.sh
@@ -6,17 +6,21 @@ set -euo pipefail
 commits=$(git log --reverse --pretty=format:"%H" $BEFORE..$AFTER -- lean-toolchain)
 for commit in $commits; do
   # Get the toolchain that this PR switched to.
-  toolchain=$(git show $commit:lean-toolchain)
+  toolchain=$(git show $commit:lean-toolchain) || {
+    echo "WARNING: Could not read lean-toolchain at commit $commit. Please avoid newlines or other special characters!"
+    continue
+  }
   # Strip the repository prefix, so we just get the suffix.
   if [[ "$toolchain" =~ ^leanprover/lean4:(.*)$ ]]; then
     toolchain_version="${BASH_REMATCH[1]}"
   else
-    echo "Not on leanprover/lean4 toolchain; not tagging commit: $commit"
+    echo "INFO: Not on leanprover/lean4 toolchain; not tagging commit: $commit"
+    continue
   fi
 
   # Only add a tag for this toolchain if we haven't added it before.
   if git ls-remote --tags --exit-code origin "$toolchain_version" >/dev/null; then
-    echo "Already have a tag for $toolchain_version; not tagging commit: $commit"
+    echo "INFO: Already have a tag for $toolchain_version; not tagging commit: $commit"
   else
     # If the tag does not exist, create and push the tag to remote
     git tag "$toolchain_version"
